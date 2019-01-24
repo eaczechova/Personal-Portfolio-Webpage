c4f4aa771abd24de5e69f025b47e306428aedf89
Ewa Czech <eaprojekty@gmail.com>
2019-01-24 21:49:20 +0100

    clean and format css and js file

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+// scroll to the top on reload
+
+window.onload = function() {
+  setTimeout (function () {
+    scrollTo(0,0);
+  }, 100);
+}
+
+// Navigation toggle
+
 let mainNav = document.getElementById('ul-menu');
 let navBarToggle = document.getElementById('menu-toggle');
 let welcomeSectionH1 = document.getElementById('welcome-section-h1');
@@ -5,12 +15,16 @@ navBarToggle.addEventListener('click', function() {
   mainNav.classList.toggle('active');
 });
 
+// Close naviagtion bar once menu item is clicked
+
 let navItem = document.getElementsByClassName('nav-item');
 for (let i = 0; i < navItem.length; i++) {
   navItem[i].addEventListener('click', function(event) {
     mainNav.classList.remove('active'); }
   )
 }
+
+//Smooth scroll to/back from anchor element
 
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener('click', function (e) {
